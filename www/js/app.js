704ee1ffa50d9be95b022dd6f7bd076f5ab705d2
javierbyte <hi@javier.xyz>
2015-09-27 15:56:44 -0500

    Update appjs on public

--- www/js/app.js
+++ www/js/app.js
@@ -3,17 +3,20 @@ function dispatch(label, optional) {
 	mixpanel.track(label);
 }
 
+function goToApp() {
+	dispatch('register-first');
+
+	var firstAmount = parseInt($('[data-start-amount]')[0].value, 10) || 0;
+	var secondAmount = parseInt($('[data-start-amount]')[1].value, 10) || 0;
+
+	var amount = Math.max(firstAmount, secondAmount, 5000);
+
+	location.href = '/app?amount=' + amount;
+}
+
 $(function() {
 	dispatch('pageview');
 
-	$('[data-start-now]').on('click', function() {
-		dispatch('register-first');
-
-		var firstAmount = parseInt($('[data-start-amount]')[0].value, 10) || 0;
-		var secondAmount = parseInt($('[data-start-amount]')[1].value, 10) || 0;
-
-		var amount = Math.max(firstAmount, secondAmount, 5000);
-
-		location.href = '/app?amount=' + amount;
-	})
+	$('[data-start-now]').on('click', goToApp);
+	$('form').on('submit', goToApp);
 });
